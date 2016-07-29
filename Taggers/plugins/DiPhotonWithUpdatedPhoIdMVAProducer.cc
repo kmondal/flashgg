9048a88d34549624dc59ecc3af796deb4517f6d3
kuntal <kuntal.mondal@cern.ch>
2016-07-29 11:03:39 +0200

    Applied correction for SigmaEOverE for both EB and EE

--- Taggers/plugins/DiPhotonWithUpdatedPhoIdMVAProducer.cc
+++ Taggers/plugins/DiPhotonWithUpdatedPhoIdMVAProducer.cc
@@ -84,12 +84,14 @@ namespace flashgg {
                     new_obj->getLeadingPhoton().full5x5_setShowerShapeVariables(newShowerShapes);
                     leadCorrectedEtaWidth = corrections_[1]->Eval(new_obj->getLeadingPhoton().superCluster()->etaWidth());
                     new_obj->getLeadingPhoton().getSuperCluster()->setEtaWidth(leadCorrectedEtaWidth);
-                    std::cout << "S4 uncorrected" << new_obj->getLeadingPhoton().s4() << std::endl;
+                    //std::cout << "S4 uncorrected" << new_obj->getLeadingPhoton().s4() << std::endl;
+                    //std::cout << "s4 actual after correction"  << corrections_[2]->Eval(new_obj->getLeadingPhoton().s4()) << std::endl; 
                     new_obj->getLeadingPhoton().setS4(corrections_[2]->Eval(new_obj->getLeadingPhoton().s4()));
-                    std::cout << "S4 corrected" << new_obj->getLeadingPhoton().s4() << std::endl;
-                    std::cout << "SigmaEoE uncorrected" << new_obj->getLeadingPhoton().sigEOverE() << std::endl;
+                    //std::cout << "S4 corrected" << new_obj->getLeadingPhoton().s4() << std::endl;
+                    //std::cout << "SigmaEoE uncorrected" << new_obj->getLeadingPhoton().sigEOverE() << std::endl;
+                    //std::cout << "SigmaEOverE actual correction" << corrections_[6]->Eval(new_obj->getLeadingPhoton().sigEOverE()) << std::endl;
                     new_obj->getLeadingPhoton().setSigEOverE(corrections_[6]->Eval(new_obj->getLeadingPhoton().sigEOverE()));
-                    std::cout << "SigmaEoE corrected" << new_obj->getLeadingPhoton().sigEOverE() << std::endl;
+                    //std::cout << "SigmaEoE corrected" << new_obj->getLeadingPhoton().sigEOverE() << std::endl;
 
                     if (this->debug_) {
                         std::cout << new_obj->getLeadingPhoton().full5x5_r9() << std::endl;
